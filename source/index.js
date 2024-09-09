c8b62758db06be3d55dc9d2bc9cfc270bbcea86f
Matthieu Viry <matthieu.viry@cnrs.fr>
2024-09-09 19:33:16 +0200

    Update source project

---
+++ source/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(e) {
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      window.applicationCache.swapCache();
+      window.location.reload();
+    }
+  }, false);
+}, false);
